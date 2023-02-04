b8f7a1580fad3af3697de3264d5576e4041d0690
Sarthak Roy <sarthakroy2002@gmail.com>
2023-02-04 23:15:30 +0530

    Add images to the Dice Roller app

--- app/src/main/java/com/neolit/diceroller/MainActivity.kt
+++ app/src/main/java/com/neolit/diceroller/MainActivity.kt
@@ -5,7 +5,7 @@ package com.neolit.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /*
@@ -19,22 +19,30 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            rollDice(R.id.DiceView1)
-            rollDice(R.id.DiceView2)
+            rollDice()
         }
     }
 
     /*
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice(DiceView: Int) {
+    private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(DiceView)
-        resultTextView.text = diceRoll.toString()
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
@@ -42,7 +50,7 @@ class MainActivity : AppCompatActivity() {
  * Dice class which will return
  * random/rolled value
  */
-class Dice(val numSides: Int) {
+class Dice(private val numSides: Int) {
 
     fun roll(): Int {
         return (1..numSides).random()
